468adaf66a9a938e9653da6d897c42cd72bdcbbf
Skaleivt <skaleiviktor@gmail.com>
2025-10-11 10:24:36 +0300

    fix mentor error

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import { contactsRouter } from './contacts.js';
 import { authRouter } from './auth.js';
 import { authorization } from '../middlewares/authenticate.js';
-// import { checkUser } from '../middlewares/checkUser.js';
 
 const router = Router();
 
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,10 +1,8 @@
 import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required().messages({
+  name: Joi.string().required().messages({
     'string.base': 'Username should be a string',
-    'string.min': 'Username should have at least {#limit} characters',
-    'string.max': 'Username should have at most {#limit} characters',
     'any.required': 'Username is required',
   }),
   email: Joi.string()
@@ -12,29 +10,21 @@ export const registerUserSchema = Joi.object({
       minDomainSegments: 2,
       tlds: { allow: ['com', 'net'] },
     })
-    .trim()
-    .lowercase()
-    .min(3)
-    .max(20)
     .required()
     .messages({
       'string.base': 'Email must be a string',
       'string.email': 'Email must be a valid address with .com or .net domain',
       'any.required': 'Email is required',
     }),
-  password: Joi.string()
-    .min(8).max(30)
-    .required()
-    .messages({
-      'string.base': 'Password must be a string.',
-      'string.empty': 'Password cannot be empty.',
-      'string.min': 'Password must be at least 8 characters long.',
-      'any.required': 'Password is required.',
-    }),
+  password: Joi.string().required().messages({
+    'string.base': 'Password must be a string.',
+    'string.empty': 'Password cannot be empty.',
+    'any.required': 'Password is required.',
+  }),
 });
 
 export const loginUserSchema = Joi.object({
-  email: Joi.string().email().trim().lowercase().required().messages({
+  email: Joi.string().email().required().messages({
     'string.empty': 'Email is required',
     'string.email': 'Please enter a valid email address',
   }),
